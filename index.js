5dff28f2a89da24c5f875988827c84b2b02dbd30
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-04-27 13:41:36 +0100

    added new eval

--- index.js
+++ index.js
@@ -61,14 +61,11 @@ app.post('/webhook', function (req, res) {
               var foo;
 
               const chooseCMS = function(requirements) {
-                // console.log(this.stack);
-                // console.log(this.brochure);
-                // console.log(this.commerce);
-                // console.log(this.features);
-                foo = "bar";
+                console.log(this.stack);
+                console.log(this.brochure);
+                console.log(this.commerce);
+                console.log(this.features);
               }();
-
-              console.log(foo);
 
               return res.json({
                 speech: 'evaluate',
