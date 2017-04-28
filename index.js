84baeff045f403b1b85bff352316760e75474bfb
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-04-28 14:55:51 +0100

    fianl evaluation

--- index.js
+++ index.js
@@ -141,6 +141,8 @@ app.post('/webhook', function (req, res) {
 
               const speech = function() {
 
+                console.log(brochure);
+
                 let brochure = (requirements.brochure) ? 'brochure' : 'non-brochure';
                 let ecommerce = (requirements.ecommerce) ? 'requires e-commerce' : 'does not require e-commerce';
                 let features = featureList || 'no features';
