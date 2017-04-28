d6fd7f3024b723b9253d4f51d4ea021925022c09
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-04-28 12:17:27 +0100

    fianl evaluation

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ app.post('/webhook', function (req, res) {
                 let features = featureList || 'no features';
 
                 if (evaluate !== 'unknown') {
-                  return  `You want a ${brochure} ${requirements.stack}-based CMS, with ${features} and ${ecommerce} functionality.\nI would definitely recommend ${evaluate}!`
+                  return  `You want a ${requirements.stack}-based ${brochure} CMS, with ${features} and ${ecommerce} functionality.\nI would definitely recommend ${evaluate}!`
                 } else {
                   return  `You want a ${brochure} CMS, with ${features} and ${ecommerce} functionality.\nI cannot think of a suitable CMS with the ${requirements.stack} tech stack you specified.\nHopefully I will learn more stacks as the app continues to learn!`
                 }
@@ -130,6 +130,7 @@ app.post('/webhook', function (req, res) {
                 speech: speech,
                 source: 'drinkabout-evaluation-cms',
                 displayText: speech
+                resetContexts: true
               });             
             }
           }
